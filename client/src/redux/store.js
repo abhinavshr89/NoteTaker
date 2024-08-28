0679278a986f598c05b4521e6a7a2fd648d650d0
abhinav <abhinav312003@gmail.com>
2024-08-28 17:12:00 +0530

    commit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import { userLoginReducer, userRegisterReducer } from '../reducers/userReducers';
+import { noteListReducer } from '../reducers/notesReducer';
 
 const reducer = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  noteList: noteListReducer,
 });
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
